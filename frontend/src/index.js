20dfa5d38eff895f6f07c672600223c6fbe0edf8
Abiy <abiyzenagabriel@gmail.come.com>
2026-03-30 16:00:17 +0300

    just starting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./homepage"; // import your new component
-import "./App.css"; // optional, keep if you have styles
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HomePage />);
